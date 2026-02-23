c2fe99f21d541178fa8bcfc3b87550cf42f1c72c
fernandoherrarte-2021072 <fherrarte-2021072@kinal.edu.gt>
2026-02-23 07:36:35 -0600

    Primer commit de Auth-Service

---
+++ auth-service-main/src/AuthService.Domain/Constants/RoleConstans.cs
@@ -0,0 +1,6 @@
+namespace AuthService.Domain.Constants;
+
+    public class Constants
+    {
+       public const string ADMIN_ROLE = "ADMIN_ROLE";
+    }
---
+++ src/AuthService.Domain/User.cs
@@ -0,0 +1,11 @@
+namespace AuthService.Domain;
+
+public class User
+{
+    public int Id { get; set; }
+    public string Username { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string PasswordHash { get; set; } = string.Empty;
+    public string Role { get; set; } = "Client"; // Admin o Client
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
